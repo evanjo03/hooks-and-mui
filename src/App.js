15c3bcce7ba817fb7fba63d7bdf8b91718e35d87
John Evans <john.wes.evans@gmail.com>
2019-08-08 21:53:26 -0500

    fixed deletion. app functionality about complete for now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 import React, {
   useState
 } from "react";
+import "./App.css"
+
+const style = {
+  button: {
+    background: "blue",
+    color: "white",
+    padding: "5px",
+    margin: "5px"
+  }
+};
+
+function Todo({ todo, index, deleteTodo }) {
+  return (
+    <div className="todo">
+      {todo.text}
+      <button style={style.button} onClick={() => deleteTodo(index)}>Delete</button>
+    </div>
+      
+  )
+}
+function Header() {
+  return (
+    <h1>To Do List</h1>
+  )
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) {
+      return;
+    }
+    addTodo(value);
+    setValue("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)}/>
+    </form>
+  )
+}
 
 function App() {
   const [todos, setTodos] = useState([{
@@ -17,14 +61,26 @@ function App() {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
+  const deleteTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index,1)
+    setTodos(newTodos)
+  }
+  
+
   return (
     <div className="app">
+      <Header/>
         <div className="todo">
-          {todos.map(todo => {
-            return(
-              <h1>{todo.text}</h1>
-            )
-          })}
+          {todos.map((todo, index) => 
+            <Todo key={index} index={index} todo={todo} deleteTodo={deleteTodo}/>
+            )}
+            <TodoForm addTodo={addTodo}/>
       </div>
     </div>
   )
